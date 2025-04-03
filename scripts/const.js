401afa8b11fe5dce51ae2a0081bc6f265d506d5f
user8949 <user8949@noreply.gitcode.com>
2025-04-03 17:20:22 +0800

    fix: 修复打包问题

--- scripts/const.js
+++ scripts/const.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export const indexFileName = 'index.ts';
-export const ignoreDirs = ['PopperTrigger', 'node_modules', 'dist'];
+export const ignoreDirs = ['node_modules', 'dist'];
 export const componentsDir = path.resolve(__dirname, '../packages/components');
 export const componentIndexFile = path.resolve(componentsDir, `./${indexFileName}`);
 export const buildLibOutputDir = path.resolve(__dirname, '../packages/components/dist');
