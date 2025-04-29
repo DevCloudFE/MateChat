b9c12cfcf1c3804c6ab9a2420fcd46d05b28ca1d
user8949 <user8949@noreply.gitcode.com>
2025-04-29 14:44:32 +0800

    feat: vue-starter大模型接入

--- packages/vue-starter/src/models/config.ts
+++ packages/vue-starter/src/models/config.ts
@@ -20,7 +20,7 @@ export const LLM_MODELS: LLMModelsConfig[] = [
     // 硅基流动
     providerKey: LLMProviders.SILICON_FLOW,
     apiPath: 'https://api.siliconflow.cn/v1',
-    apiKey: 'sk-gshxufdodqxappusrorsnxlycysazdiowmvjnvnlbuznvngm',
+    apiKey: '',
     models: ['deepseek-ai/DeepSeek-R1'],
     available: true,
     clientKey: LLMClientKey.openai,
@@ -29,7 +29,7 @@ export const LLM_MODELS: LLMModelsConfig[] = [
     // deepseek
     providerKey: LLMProviders.DEEP_SEEK,
     apiPath: 'https://api.deepseek.com',
-    apiKey: 'sk-d6aaf62a31fa44e7b4cc308746a876cc',
+    apiKey: '',
     models: ['deepseek-reasoner'],
     available: true,
     clientKey: LLMClientKey.openai,
@@ -38,7 +38,7 @@ export const LLM_MODELS: LLMModelsConfig[] = [
     // 通义千问
     providerKey: LLMProviders.QWEN,
     apiPath: 'https://dashscope.aliyuncs.com/compatible-mode/v1',
-    apiKey: 'sk-1da33fb5253e4bc884b3e0c167012d65',
+    apiKey: '',
     models: ['qwen-plus'],
     available: true,
     clientKey: LLMClientKey.openai,
