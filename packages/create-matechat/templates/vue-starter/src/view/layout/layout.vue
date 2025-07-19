6039a6df033c4ecab8fc79a60f75ec15014fe6d2
Zlc20030208 <3043726751@qq.com>
2025-07-19 22:27:08 +0800

    fix:修复功能区整体偏右的问题

--- packages/create-matechat/templates/vue-starter/src/view/layout/layout.vue
+++ packages/create-matechat/templates/vue-starter/src/view/layout/layout.vue
@@ -10,22 +10,20 @@
 </template>
 
 <script setup lang="ts">
-import GlobalConfig from "@/global-config";
+import GlobalConfig from '@/global-config';
 </script>
 
 <style scoped lang="scss">
 @import "devui-theme/styles-var/devui-var.scss";
 
 .matechat-layout {
-  width: 100%;
-  height: 100vh;
-  padding: 8px 8px 8px 0;
-  overflow: auto;
-  box-sizing: border-box;
-}
-.aside-content:not(.Assistant) {
-  padding: 0 8px;
-}
+    width: 100%;
+    height: 100vh;
+    padding: 8px;
+    overflow: auto;
+    box-sizing: border-box;
+  }
+/* 已移除侧边栏相关样式 */
 .main-content {
   position: relative;
   flex: 1;
