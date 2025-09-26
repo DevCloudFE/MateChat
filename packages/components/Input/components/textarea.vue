3413361352c2e3b18eec9cc9610b57079d473bd1
CoderMrLuo <CoderMrLuo@noreply.gitcode.com>
2025-09-26 16:04:50 +0800

    fix: 修复input组件在safari浏览器下，中文输入法下选词过程中，按enter会直接发送的问题
    Signed-off-by: CoderMrLuo <CoderMrLuo@noreply.gitcode.com>

--- packages/components/Input/components/textarea.vue
+++ packages/components/Input/components/textarea.vue
@@ -60,8 +60,10 @@ const onCompositionStart = () => {
   lock = true;
 };
 const onCompositionEnd = () => {
-  lock = false;
-  emitChange();
+  setTimeout(()=>{
+    lock = false;
+    emitChange();
+  },10);
 };
 
 const onKeydown = (e: KeyboardEvent) => {
