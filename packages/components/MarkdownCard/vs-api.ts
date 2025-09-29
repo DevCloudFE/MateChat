2da5ee20318e90ed23bb3fc9fd4cd9a529a404ec
苏向夜 <fu050409@163.com>
2025-09-29 10:00:18 +0800

    fix: fix vscode types def

--- packages/components/MarkdownCard/vs-api.ts
+++ packages/components/MarkdownCard/vs-api.ts
@@ -1,2 +1,4 @@
-export const vscode = acquireVsCodeApi?.();
+import type { WebviewApi } from 'vscode-webview';
+
+export const vscode: WebviewApi<unknown> | undefined = acquireVsCodeApi?.();
 export const isVscode = typeof vscode !== 'undefined';
