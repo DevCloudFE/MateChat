c34a9ee18767fd51587823c57daa1b9510fc3c7d
gellenliu <1042382110@qq.com>
2025-10-10 15:32:59 +0800

    feat: ng图标样式

--- packages/components-ng/projects/demo-app/src/main.ts
+++ packages/components-ng/projects/demo-app/src/main.ts
@@ -2,7 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { ThemeServiceInit, infinityTheme } from 'devui-theme';
-import '@devui-design/icons/icomoon/devui-icon.css';
  
 // 使用无限主题
 ThemeServiceInit({ infinityTheme }, 'infinityTheme');
