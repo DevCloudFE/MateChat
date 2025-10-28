3444916e4060e24294ea4c860660bbf10a6f9631
苏向夜 <fu050409@163.com>
2025-10-28 21:39:13 +0800

    revert(format): revert code format

--- packages/components/MarkdownCard/CodeBlock.vue
+++ packages/components/MarkdownCard/CodeBlock.vue
@@ -6,45 +6,69 @@
         <div class="mc-code-block-actions">
           <div v-if="isMermaid" style="margin-right: 8px">
             <ul class="mc-diagram-switch" :class="{ 'mc-show-code': !showMermaidDiagram }">
-              <li @click="showMermaidDiagram = true" :class="{ 'mc-diagram-switch-active': showMermaidDiagram }">{{
-                t('Md.diagram') }}</li>
-              <li @click="showMermaidDiagram = false" :class="{ 'mc-diagram-switch-active': !showMermaidDiagram }">{{
-                t('Md.code') }}</li>
+              <li @click="showMermaidDiagram = true" :class="{ 'mc-diagram-switch-active': showMermaidDiagram }">{{ t('Md.diagram') }}</li>
+              <li @click="showMermaidDiagram = false" :class="{ 'mc-diagram-switch-active': !showMermaidDiagram }">{{ t('Md.code') }}</li>
             </ul>
           </div>
-          <div v-if="isMermaid && showMermaidDiagram" class="mc-action-btn mc-toggle-btn" :title="t('Md.zoomIn')"
-            @click="zoomIn">
+          <div
+            v-if="isMermaid && showMermaidDiagram"
+            class="mc-action-btn mc-toggle-btn"
+            :title="t('Md.zoomIn')"
+            @click="zoomIn"
+          >
             <img src="./asset/enlarge.svg" />
           </div>
-          <div v-if="isMermaid && showMermaidDiagram" class="mc-action-btn mc-toggle-btn" :title="t('Md.zoomOut')"
-            @click="zoomOut">
+          <div
+            v-if="isMermaid && showMermaidDiagram"
+            class="mc-action-btn mc-toggle-btn"
+            :title="t('Md.zoomOut')"
+            @click="zoomOut"
+          >
             <img src="./asset/zoom-out-2.svg" />
           </div>
-          <div v-if="isMermaid && showMermaidDiagram" class="mc-action-btn mc-toggle-btn" :title="t('Md.downLoad')"
-            @click="download">
+          <div
+            v-if="isMermaid && showMermaidDiagram"
+            class="mc-action-btn mc-toggle-btn"
+            :title="t('Md.downLoad')"
+            @click="download"
+          >
             <img src="./asset/download-2.svg" />
           </div>
-          <div class="mc-action-btn mc-toggle-btn" :title="t('Md.toggle')" @click="toggleExpand">
+          <div
+            class="mc-action-btn mc-toggle-btn"
+            :title="t('Md.toggle')"
+            @click="toggleExpand"
+          >
             <img src="./asset/all-collapse.svg" />
           </div>
-          <div class="mc-action-btn mc-copy-btn" :title="t('Md.copy')" @click="copyCode">
+          <div
+            class="mc-action-btn mc-copy-btn"
+            :title="t('Md.copy')"
+            @click="copyCode"
+          >
             <img v-if="!copied" src="./asset/copy-new.svg" />
             <img v-else src="./asset/right.svg">
           </div>
-          <div class="mc-action-btn mc-copy-btn" :title="t('Md.apply')" @click="applyCode" v-if="isVscode">
-            <img src="./asset/add-requestor.svg" />
-          </div>
+        </div>
+        <div class="mc-action-btn mc-copy-btn" :title="t('Md.apply')" @click="applyCode" v-if="isVscode">
+          <img src="./asset/add-requestor.svg" />
         </div>
       </slot>
     </div>
     <slot name="header" v-else></slot>
-    <Transition name="collapse-transition" @beforeEnter="beforeEnter" @enter="enter" @afterEnter="afterEnter"
-      @beforeLeave="beforeLeave" @leave="leave" @afterLeave="afterLeave">
+    <Transition
+      name="collapse-transition"
+      @beforeEnter="beforeEnter"
+      @enter="enter"
+      @afterEnter="afterEnter"
+      @beforeLeave="beforeLeave"
+      @leave="leave"
+      @afterLeave="afterLeave"
+    >
       <div v-show="expanded">
-        <div v-if="isMermaid && showMermaidDiagram && !$slots.content" class="mc-mermaid-content"></div>
-        <pre
-          v-else-if="!$slots.content"><code :class="`hljs language-${language}`" v-html="highlightedCode"></code></pre>
-        <slot v-else name="content"></slot>
+          <div v-if="isMermaid && showMermaidDiagram && !$slots.content" class="mc-mermaid-content"></div>
+          <pre v-else-if="!$slots.content"><code :class="`hljs language-${language}`" v-html="highlightedCode"></code></pre>
+          <slot v-else name="content"></slot>
       </div>
     </Transition>
   </div>
@@ -65,20 +89,20 @@ type MermaidConfig = {
 const props = defineProps({
   code: {
     type: String,
-    required: true
+      required: true
   },
   language: {
     type: String,
-    default: ''
+      default: ''
   },
   blockIndex: {
     type: Number,
-    required: true
+      required: true
   },
   theme: {
     type: String,
-    default: 'light'
-  },
+      default: 'light'
+    },
   enableMermaid: {
     type: Boolean,
     default: false
@@ -107,14 +131,14 @@ const highlightedCode = computed(() => {
     let content;
     if (props.language && hljs.getLanguage(props.language)) {
       if (typeIndex !== -1) {
-        content = hljs.highlight(props.code.slice(0, typeIndex), { language: props.language }).value + props.code.slice(typeIndex);
+            content = hljs.highlight(props.code.slice(0, typeIndex), { language: props.language }).value + props.code.slice(typeIndex);
       } else {
-        content = hljs.highlight(props.code, { language: props.language }).value;
+            content = hljs.highlight(props.code, { language: props.language }).value;
       }
     } else {
       if (typeof hljs.highlightAuto !== "undefined") {
         if (typeIndex !== -1) {
-          content = hljs.highlightAuto(props.code.slice(0, typeIndex)).value + props.code.slice(typeIndex);
+                content = hljs.highlightAuto(props.code.slice(0, typeIndex)).value + props.code.slice(typeIndex);
         } else {
           content = hljs.highlightAuto(props.code).value;
         }
@@ -123,7 +147,7 @@ const highlightedCode = computed(() => {
       }
     }
     return content;
-  } catch (_) { }
+  } catch (_) {}
 });
 
 const rootRef = ref<HTMLElement | null>(null);
@@ -181,18 +205,18 @@ const copyCode = debounce((e: Event) => {
   if (navigator.clipboard) {
     navigator.clipboard.writeText(props.code);
   } else {
-    const textarea = document.createElement('textarea');
-    textarea.style.position = 'fixed';
-    textarea.style.top = '-9999px';
-    textarea.style.left = '-9999px';
-    textarea.style.zIndex = '-1';
+      const textarea = document.createElement('textarea');
+      textarea.style.position = 'fixed';
+      textarea.style.top = '-9999px';
+      textarea.style.left = '-9999px';
+      textarea.style.zIndex = '-1';
     textarea.value = props.code;
     document.body.appendChild(textarea);
     textarea.select();
-    document.execCommand('copy');
+      document.execCommand('copy');
     document.body.removeChild(textarea);
   }
-  target.classList.remove('icon-copy-new');
+    target.classList.remove('icon-copy-new');
   copied.value = true;
   setTimeout(() => {
     copied.value = false;
@@ -225,11 +249,11 @@ const enter = (el: RendererElement) => {
     } else {
       el.style.maxHeight = 0;
     }
-    el.style.overflow = 'hidden';
+      el.style.overflow = 'hidden';
   });
 };
 const afterEnter = (el: RendererElement) => {
-  el.style.maxHeight = '';
+    el.style.maxHeight = '';
   el.style.overflow = el.dataset.oldOverflow;
 };
 const beforeLeave = (el: RendererElement) => {
@@ -238,7 +262,7 @@ const beforeLeave = (el: RendererElement) => {
   }
   el.dataset.oldOverflow = el.style.overflow;
   el.style.maxHeight = `${el.scrollHeight}px`;
-  el.style.overflow = 'hidden';
+    el.style.overflow = 'hidden';
 };
 const leave = (el: RendererElement) => {
   if (el.scrollHeight !== 0) {
@@ -246,12 +270,12 @@ const leave = (el: RendererElement) => {
   }
 };
 const afterLeave = (el: RendererElement) => {
-  el.style.maxHeight = '';
+    el.style.maxHeight = '';
   el.style.overflow = el.dataset.oldOverflow;
 };
 
 const themeClass = computed(() => {
-  return props.theme === 'dark' ? 'mc-code-block-dark' : 'mc-code-block-light';
+    return props.theme === 'dark' ? 'mc-code-block-dark' : 'mc-code-block-light';
 });
 
 watch(() => [props.code, props.theme, props.enableMermaid], () => {
@@ -283,11 +307,9 @@ onMounted(() => {
 <style scoped lang="scss">
 @use 'devui-theme/styles-var/devui-var.scss' as *;
 @use "sass:meta";
-
 .mc-code-block-light :deep() {
   @include meta.load-css("highlight.js/styles/a11y-light");
 }
-
 .mc-code-block-dark :deep() {
   @include meta.load-css("highlight.js/styles/a11y-dark");
 }
@@ -321,7 +343,6 @@ onMounted(() => {
     justify-content: space-between;
     align-items: center;
     padding: 0.5rem 1rem;
-
     .mc-code-lang {
       font-size: var(--devui-font-size, 14px);
     }
@@ -338,7 +359,6 @@ onMounted(() => {
   .mc-code-block-actions {
     display: flex;
     align-items: center;
-
     .mc-copy-btn,
     .mc-toggle-btn {
       cursor: pointer;
@@ -347,7 +367,6 @@ onMounted(() => {
       padding: 4px;
     }
   }
-
   .mc-diagram-switch {
     display: flex;
     align-items: center;
@@ -405,29 +424,22 @@ onMounted(() => {
 
 .mc-code-block-light {
   border: 1px solid #d7d8da;
-
   code.hljs {
     padding: 1em;
   }
-
   background-color: #f5f5f5;
-
   .mc-code-lang {
     color: #252b3a;
   }
-
   .mc-code-block-actions {
-
     .mc-copy-btn,
     .mc-toggle-btn {
       color: #252b3a;
-
       &:hover {
-        background-color: #EBEBEB;
+          background-color: #EBEBEB;
       }
     }
   }
-
   .mc-mermaid-content {
     background: #fefefe;
   }
@@ -435,40 +447,31 @@ onMounted(() => {
 
 .mc-code-block-dark {
   border: 1px solid #4e5057;
-
   code.hljs {
     padding: 1em;
   }
-
-  background-color: #34363A;
-
+    background-color: #34363A;
   .mc-code-lang {
-    color: #CED1DB;
+      color: #CED1DB;
   }
-
   .mc-code-block-actions {
-
     .mc-copy-btn,
     .mc-toggle-btn {
-      color: #CED1DB;
-
+        color: #CED1DB;
       &:hover {
         background-color: #393a3e;
       }
-
       img {
         filter: brightness(1.5);
       }
     }
   }
-
   .mc-mermaid-content {
     background: #2b2b2b;
   }
 }
 
 .collapse-transition {
-
   &-enter-from,
   &-leave-to {
     opacity: 0;
@@ -481,8 +484,8 @@ onMounted(() => {
 
   &-enter-active,
   &-leave-active {
-    transition:
-      max-height 0.3s cubic-bezier(0.5, 0.05, 0.5, 0.95),
+      transition:
+        max-height 0.3s cubic-bezier(0.5, 0.05, 0.5, 0.95),
       opacity 0.3s cubic-bezier(0.5, 0.05, 0.5, 0.95);
   }
 }
